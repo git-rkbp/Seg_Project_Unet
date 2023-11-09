ba661c0c808fb7d19b47f3f4930576b2425bc6c3
git-rkbp <rogerio.bombardelli@gmail.com>
2023-11-09 11:20:07 +0100

    adding a simple training loop

--- main.py
+++ main.py
@@ -4,24 +4,54 @@ from torchvision import models
 from torch.nn.functional import relu
 from torch.utils.data import Dataset, DataLoader,random_split
 from sklearn.model_selection import train_test_split
+import torch.optim as optim
 
 
+
+from unet import UNet_256
 from aux_seg import *
 
 
+# class CustomDataset(Dataset):
+#     def __init__(self, images, masks, transform=None):
+    
+#         """
+#         # # Assuming you have your images and masks in two numpy arrays:
+#         # # images_array: (250, 256, 256)
+#         # # masks_array: (250, 256, 256)
+
+#         Args:
+#             images (numpy.ndarray): A numpy array of shape (N, H, W) containing the images.
+#             masks (numpy.ndarray): A numpy array of shape (N, H, W) containing the segmentation masks.
+#             transform (callable, optional): Optional transform to be applied on a sample.
+#         """
+#         self.images = images
+#         self.masks = masks
+#         self.transform = transform
+
+#     def __len__(self):
+#         return len(self.images)
+
+#     def __getitem__(self, idx):
+#         image = self.images[idx]
+#         mask = self.masks[idx]
+
+#         # Add channel dimension: PyTorch expects images in the shape (C, H, W)
+#         image = np.expand_dims(image, axis=0)
+#         mask = np.expand_dims(mask, axis=0)
+
+#         sample = {'image': image, 'mask': mask}
+
+#         if self.transform:
+#             sample = self.transform(sample)
+
+#         # Convert to PyTorch tensors
+#         sample['image'] = torch.from_numpy(sample['image']).float()
+#         sample['mask'] = torch.from_numpy(sample['mask']).long()  # Assuming mask has integer type labels
+
+#         return sample['image'], sample['mask']
 class CustomDataset(Dataset):
     def __init__(self, images, masks, transform=None):
-    
-        """
-        # # Assuming you have your images and masks in two numpy arrays:
-        # # images_array: (250, 256, 256)
-        # # masks_array: (250, 256, 256)
-
-        Args:
-            images (numpy.ndarray): A numpy array of shape (N, H, W) containing the images.
-            masks (numpy.ndarray): A numpy array of shape (N, H, W) containing the segmentation masks.
-            transform (callable, optional): Optional transform to be applied on a sample.
-        """
         self.images = images
         self.masks = masks
         self.transform = transform
@@ -33,20 +63,23 @@ class CustomDataset(Dataset):
         image = self.images[idx]
         mask = self.masks[idx]
 
-        # Add channel dimension: PyTorch expects images in the shape (C, H, W)
         image = np.expand_dims(image, axis=0)
-        mask = np.expand_dims(mask, axis=0)
+
+        # Ensure that the mask has the correct shape [batch_size, height, width]
+        if mask.shape[0] == 1:
+            mask = mask[0]
 
         sample = {'image': image, 'mask': mask}
 
         if self.transform:
             sample = self.transform(sample)
 
-        # Convert to PyTorch tensors
         sample['image'] = torch.from_numpy(sample['image']).float()
-        sample['mask'] = torch.from_numpy(sample['mask']).long()  # Assuming mask has integer type labels
+        sample['mask'] = torch.from_numpy(sample['mask']).long()
 
         return sample['image'], sample['mask']
+
+
 
 
 im, masks_array = read_train_all()
@@ -75,7 +108,6 @@ val_loader = DataLoader(val_dataset, batch_size=4, shuffle=False, num_workers=2)
 
 
 # test 
-
 print(f"Total dataset size: {dataset_size}")
 print(f"Training dataset size: {len(train_dataset)}")
 print(f"Validation dataset size: {len(val_dataset)}")
@@ -94,19 +126,50 @@ print(f"Shape of validation images: {val_images.shape}")
 print(f"Shape of validation masks: {val_masks.shape}")
 
 
-# Check if CUDA is available
-is_cuda_available = torch.cuda.is_available()
-print(f"CUDA available: {is_cuda_available}")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
 
-# If CUDA is available, print the GPU name (or names, if there are multiple GPUs)
-if is_cuda_available:
-    gpu_count = torch.cuda.device_count()
-    print(f"Number of GPUs: {gpu_count}")
-    for i in range(gpu_count):
-        gpu_name = torch.cuda.get_device_name(i)
-        print(f"GPU {i} Name: {gpu_name}")
-else:
-    print("No CUDA-enabled GPU is available.")
+
+# Create an instance of your UNet model
+model = UNet_256(n_class=5).to(device)
+
+# Define the loss function (e.g., Cross-Entropy Loss)
+criterion = nn.CrossEntropyLoss()
+
+# Define the optimizer (e.g., Adam)
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+
+# Training loop
+num_epochs = 10
+for epoch in range(num_epochs):
+    model.train()  # Set the model in training mode
+
+    running_loss = 0.0
+
+    for batch_idx, (inputs, labels) in enumerate(train_loader):
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+
+        # Forward pass
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+
+        # Backward pass and optimization
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+    # Calculate average loss for the epoch
+    avg_loss = running_loss / len(train_loader)
+
+    # Print the loss for the current epoch
+    print(f"Epoch {epoch+1}/{num_epochs}, Loss: {avg_loss:.4f}")
+
+print("Training finished")
+
 
 
 
