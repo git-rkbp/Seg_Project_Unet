d913d72e2c97bdaae1576ba8f8ed8bf899ce568d
git-rkbp <rogerio.bombardelli@gmail.com>
2023-11-09 17:53:17 +0100

    a

--- main.py
+++ main.py
@@ -12,44 +12,6 @@ from unet import UNet_256
 from aux_seg import *
 
 
-# class CustomDataset(Dataset):
-#     def __init__(self, images, masks, transform=None):
-    
-#         """
-#         # # Assuming you have your images and masks in two numpy arrays:
-#         # # images_array: (250, 256, 256)
-#         # # masks_array: (250, 256, 256)
-
-#         Args:
-#             images (numpy.ndarray): A numpy array of shape (N, H, W) containing the images.
-#             masks (numpy.ndarray): A numpy array of shape (N, H, W) containing the segmentation masks.
-#             transform (callable, optional): Optional transform to be applied on a sample.
-#         """
-#         self.images = images
-#         self.masks = masks
-#         self.transform = transform
-
-#     def __len__(self):
-#         return len(self.images)
-
-#     def __getitem__(self, idx):
-#         image = self.images[idx]
-#         mask = self.masks[idx]
-
-#         # Add channel dimension: PyTorch expects images in the shape (C, H, W)
-#         image = np.expand_dims(image, axis=0)
-#         mask = np.expand_dims(mask, axis=0)
-
-#         sample = {'image': image, 'mask': mask}
-
-#         if self.transform:
-#             sample = self.transform(sample)
-
-#         # Convert to PyTorch tensors
-#         sample['image'] = torch.from_numpy(sample['image']).float()
-#         sample['mask'] = torch.from_numpy(sample['mask']).long()  # Assuming mask has integer type labels
-
-#         return sample['image'], sample['mask']
 class CustomDataset(Dataset):
     def __init__(self, images, masks, transform=None):
         self.images = images
@@ -145,38 +107,39 @@ criterion = nn.CrossEntropyLoss()
 # Define the optimizer (e.g., Adam)
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
+# Define the LR scheduler
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
 
 # Training loop
-num_epochs = 10
+num_epochs = 50
 for epoch in range(num_epochs):
     model.train()  # Set the model in training mode
-
     running_loss = 0.0
-
-    for batch_idx, (inputs, labels) in enumerate(train_loader):
+    for inputs, labels in train_loader:
         inputs, labels = inputs.to(device), labels.to(device)
-
         optimizer.zero_grad()
-
-        # Forward pass
         outputs = model(inputs)
         loss = criterion(outputs, labels)
-
-        # Backward pass and optimization
         loss.backward()
         optimizer.step()
-
         running_loss += loss.item()
+    avg_train_loss = running_loss / len(train_loader)
 
-    # Calculate average loss for the epoch
-    avg_loss = running_loss / len(train_loader)
+    # LR scheduler step
+    scheduler.step()
+    
+    # Validation part
+    model.eval()  # Set the model to evaluation mode
+    running_val_loss = 0.0
+    with torch.no_grad():  # No need to track the gradients
+        for inputs, labels in val_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            running_val_loss += loss.item()
+    avg_val_loss = running_val_loss / len(val_loader)
 
-    # Print the loss for the current epoch
-    print(f"Epoch {epoch+1}/{num_epochs}, Loss: {avg_loss:.4f}")
+    print(f"Epoch {epoch+1}/{num_epochs}, Training Loss: {avg_train_loss:.4f}, Validation Loss: {avg_val_loss:.4f}, Learning Rate: {scheduler.get_last_lr()[0]}")
 
 print("Training finished")
-
 torch.save(model.state_dict(), './checkpoint/model.pth')
-
-
-
